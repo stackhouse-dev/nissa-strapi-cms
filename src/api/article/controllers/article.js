02948bcd18dda503654282b9abbd1d18a7629c92
Alessandro Dess <alessandro.dessi@spindox.it>
2024-05-31 17:47:17 +0200

    fix only item deep response

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -10,7 +10,7 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::article.article', ({ strapi }) => ({
   async find(ctx) {
-    const entries = await strapi.entityService.findMany(
+    const entries = await strapi.entityService.findOne(
       'api::article.article',
       {
         // to avoid strange queries in FE
@@ -21,5 +21,18 @@ module.exports = createCoreController('api::article.article', ({ strapi }) => ({
     const sanitizedEntries = await this.sanitizeOutput(entries, ctx);
     return sanitizedEntries
   },
+
+  async findOne(ctx) {
+    // Filter by the current user as the author
+    ctx.query = {
+      ...ctx.query,
+      populate: 'deep'
+    };
+    // here we can manipulate response
+    const response = await super.findOne(ctx);
+  
+    return response;
+  },
+  
 
 }));
--- src/api/calendar/controllers/calendar.js
+++ src/api/calendar/controllers/calendar.js
@@ -18,8 +18,21 @@ module.exports = createCoreController('api::calendar.calendar', ({ strapi }) => 
       }
     );
     // here we can manipulate response
+
     const sanitizedEntries = await this.sanitizeOutput(entries, ctx);
     return sanitizedEntries
+  },
+
+  async findOne(ctx) {
+    // Filter by the current user as the author
+    ctx.query = {
+      ...ctx.query,
+      populate: 'deep'
+    };
+    // here we can manipulate response
+    const response = await super.findOne(ctx);
+  
+    return response;
   },
 
 }));
