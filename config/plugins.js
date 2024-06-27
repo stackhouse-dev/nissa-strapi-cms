033743c9c50583c574f590c3c21a1ccb3719791e
Alessandro Dess <alessandro.dessi@spindox.it>
2024-06-27 09:32:11 +0200

    setup plugin vercel

--- config/plugins.js
+++ config/plugins.js
@@ -10,5 +10,13 @@ module.exports = ({ env }) => ({
  },
  "vercel-deploy": {
   enabled: true,
+  config: {
+    deployHook:
+      "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+    apiToken: "B5FVKIRhvU8jfvuK4uWTyeAG",
+    appFilter: "nissaCms",
+    teamFilter: "nissaTeam",
+    roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+  },
 },
 });
